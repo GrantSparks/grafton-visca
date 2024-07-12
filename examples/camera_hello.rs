ffba90ef429b352dc9682c13a5c2c19f44cc21c0
Grant Sparks <grant@sparks.to>
2024-07-12 14:14:43 +1000

    Improved example

--- examples/camera_hello.rs
+++ examples/camera_hello.rs
@@ -1,144 +1,9 @@
-use grafton_visca::visca_command::{PanTiltDirection, ViscaCommand};
-use log::{debug, error, info};
+use grafton_visca::visca_command::{PanSpeed, PanTiltDirection, TiltSpeed, ViscaCommand};
+use grafton_visca::{send_command_and_wait, TcpTransport, UdpTransport, ViscaTransport};
+use log::{debug, info};
 use std::env;
-use std::{
-    io::{self, Read as _, Write as _},
-    net::{TcpStream, UdpSocket},
-    time::Duration,
-};
-
-trait ViscaTransport {
-    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()>;
-}
-
-struct UdpTransport {
-    socket: UdpSocket,
-    address: String,
-}
-
-impl UdpTransport {
-    fn new(address: &str) -> io::Result<Self> {
-        let socket = UdpSocket::bind("0.0.0.0:0")?;
-        socket.set_read_timeout(Some(Duration::from_secs(10)))?;
-        socket.set_write_timeout(Some(Duration::from_secs(10)))?;
-        Ok(Self {
-            socket,
-            address: address.to_string(),
-        })
-    }
-}
-
-impl ViscaTransport for UdpTransport {
-    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()> {
-        let command_bytes = command.to_bytes();
-        debug!("Command bytes: {:02X?}", command_bytes);
-        self.socket.send_to(&command_bytes, &self.address)?;
-        info!("Sent {} bytes to {}", command_bytes.len(), self.address);
-
-        let mut buffer = [0u8; 1024];
-        match self.socket.recv_from(&mut buffer) {
-            Ok((bytes_received, src)) => {
-                info!(
-                    "Received {} bytes from {}: {:02X?}",
-                    bytes_received,
-                    src,
-                    &buffer[..bytes_received]
-                );
-                display_camera_response(&buffer[..bytes_received]);
-            }
-            Err(e) => {
-                error!("Failed to receive response: {}", e);
-            }
-        }
-        Ok(())
-    }
-}
-
-struct TcpTransport {
-    stream: TcpStream,
-}
-
-impl TcpTransport {
-    fn new(address: &str) -> io::Result<Self> {
-        let stream = TcpStream::connect(address)?;
-        stream.set_read_timeout(Some(Duration::from_secs(10)))?;
-        stream.set_write_timeout(Some(Duration::from_secs(10)))?;
-        Ok(Self { stream })
-    }
-}
-
-impl ViscaTransport for TcpTransport {
-    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()> {
-        let command_bytes = command.to_bytes();
-        debug!("Command bytes: {:02X?}", command_bytes);
-        self.stream.write_all(&command_bytes)?;
-        info!("Sent {} bytes", command_bytes.len());
-
-        let mut buffer = [0u8; 1024];
-        match self.stream.read(&mut buffer) {
-            Ok(bytes_received) => {
-                info!(
-                    "Received {} bytes: {:02X?}",
-                    bytes_received,
-                    &buffer[..bytes_received]
-                );
-                display_camera_response(&buffer[..bytes_received]);
-            }
-            Err(e) => {
-                error!("Failed to receive response: {}", e);
-            }
-        }
-        Ok(())
-    }
-}
-
-fn display_camera_response(response: &[u8]) {
-    let mut i = 0;
-    while i < response.len() {
-        if response[i] == 0x90 {
-            match response.get(i..i + 3) {
-                Some([0x90, code, 0xFF]) if (0x40..=0x4F).contains(code) => {
-                    info!("ACK: Command accepted with status code: {:02X?}", code);
-                    i += 3;
-                }
-                Some([0x90, code, 0xFF]) if (0x50..=0x5F).contains(code) => {
-                    info!(
-                        "Completion: Command executed with status code: {:02X?}",
-                        code
-                    );
-                    i += 3;
-                }
-                Some([0x90, 0x60, 0x02, 0xFF]) => {
-                    error!("Error: Syntax Error.");
-                    i += 4;
-                }
-                Some([0x90, 0x60, 0x03, 0xFF]) => {
-                    error!("Error: Command Buffer Full.");
-                    i += 4;
-                }
-                Some([0x90, code, 0x04, 0xFF]) if (0x60..=0x6F).contains(code) => {
-                    error!("Error: Command Canceled.");
-                    i += 4;
-                }
-                Some([0x90, code, 0x05, 0xFF]) if (0x60..=0x6F).contains(code) => {
-                    error!("Error: No Socket.");
-                    i += 4;
-                }
-                Some([0x90, code, 0x41, 0xFF]) if (0x60..=0x6F).contains(code) => {
-                    error!("Error: Command Not Executable.");
-                    i += 4;
-                }
-                _ => {
-                    error!("Unknown response: {:02X?}", &response[i..i + 3]);
-                    i += 3;
-                }
-            }
-        } else {
-            error!("Unexpected response format: {:02X?}", &response[i..]);
-            break;
-        }
-    }
-}
+use std::io;
+use std::time::Duration;
 
 fn main() -> io::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
@@ -158,33 +23,33 @@ fn main() -> io::Result<()> {
         Box::new(TcpTransport::new(tcp_address)?)
     };
 
+    debug!("Sending Pan/Tilt home command");
     let pan_tilt_home_command = ViscaCommand::PanTiltHome;
-    match transport.send_command(&pan_tilt_home_command) {
-        Ok(_) => info!("Pan tilt home command sent successfully."),
-        Err(e) => error!("Failed to send pan tilt home command: {}", e),
-    }
+    send_command_and_wait(&mut transport, &pan_tilt_home_command)?;
 
-    let pan_tilt_up_command = ViscaCommand::PanTiltDrive(PanTiltDirection::Down, 0x10, 0x10);
+    let pan_tilt_up_command = ViscaCommand::PanTiltDrive(
+        PanTiltDirection::Down,
+        PanSpeed::new(0x10).unwrap(),
+        TiltSpeed::new(0x10).unwrap(),
+    );
     let start_time = std::time::Instant::now();
     while start_time.elapsed() < Duration::from_secs(3) {
-        match transport.send_command(&pan_tilt_up_command) {
-            Ok(_) => info!("Command sent successfully."),
-            Err(e) => error!("Failed to send command: {}", e),
-        }
-        std::thread::sleep(Duration::from_millis(500));
+        debug!("Sending Pan/Tilt up command");
+        send_command_and_wait(&mut transport, &pan_tilt_up_command)?;
+        std::thread::sleep(Duration::from_millis(1000));
     }
 
-    let stop_command = ViscaCommand::PanTiltDrive(PanTiltDirection::Stop, 0x00, 0x00);
-    match transport.send_command(&stop_command) {
-        Ok(_) => info!("Stop command sent successfully."),
-        Err(e) => error!("Failed to send stop command: {}", e),
-    }
+    let stop_command = ViscaCommand::PanTiltDrive(
+        PanTiltDirection::Stop,
+        PanSpeed::new(0x00).unwrap(),
+        TiltSpeed::new(0x00).unwrap(),
+    );
+    debug!("Sending Pan/Tilt stop command");
+    send_command_and_wait(&mut transport, &stop_command)?;
 
     let pan_tilt_home_command = ViscaCommand::PanTiltHome;
-    match transport.send_command(&pan_tilt_home_command) {
-        Ok(_) => info!("Pan tilt home command sent successfully."),
-        Err(e) => error!("Failed to send pan tilt home command: {}", e),
-    }
+    debug!("Sending Pan/Tilt home command");
+    send_command_and_wait(&mut transport, &pan_tilt_home_command)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,174 @@
 pub mod visca_command;
+use log::{debug, error, info};
+use std::{
+    io::{self, Read as _, Write as _},
+    net::{TcpStream, UdpSocket},
+    time::Duration,
+};
+use visca_command::ViscaCommand;
+
+pub trait ViscaTransport {
+    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()>;
+    fn receive_response(&mut self) -> io::Result<Vec<u8>>;
+}
+
+pub struct UdpTransport {
+    socket: UdpSocket,
+    address: String,
+}
+
+impl UdpTransport {
+    pub fn new(address: &str) -> io::Result<Self> {
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        socket.set_read_timeout(Some(Duration::from_secs(10)))?;
+        socket.set_write_timeout(Some(Duration::from_secs(10)))?;
+        Ok(Self {
+            socket,
+            address: address.to_string(),
+        })
+    }
+}
+
+impl ViscaTransport for UdpTransport {
+    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()> {
+        let command_bytes = command.to_bytes();
+        debug!("Command bytes: {:02X?}", command_bytes);
+        self.socket.send_to(&command_bytes, &self.address)?;
+        info!("Sent {} bytes to {}", command_bytes.len(), self.address);
+        Ok(())
+    }
+
+    fn receive_response(&mut self) -> io::Result<Vec<u8>> {
+        let mut buffer = [0u8; 1024];
+        match self.socket.recv_from(&mut buffer) {
+            Ok((bytes_received, src)) => {
+                info!(
+                    "Received {} bytes from {}: {:02X?}",
+                    bytes_received,
+                    src,
+                    &buffer[..bytes_received]
+                );
+                Ok(buffer[..bytes_received].to_vec())
+            }
+            Err(e) => {
+                error!("Failed to receive response: {}", e);
+                Err(e)
+            }
+        }
+    }
+}
+
+pub struct TcpTransport {
+    stream: TcpStream,
+}
+
+impl TcpTransport {
+    pub fn new(address: &str) -> io::Result<Self> {
+        let stream = TcpStream::connect(address)?;
+        stream.set_read_timeout(Some(Duration::from_secs(10)))?;
+        stream.set_write_timeout(Some(Duration::from_secs(10)))?;
+        Ok(Self { stream })
+    }
+}
+
+impl ViscaTransport for TcpTransport {
+    fn send_command(&mut self, command: &ViscaCommand) -> io::Result<()> {
+        let command_bytes = command.to_bytes();
+        debug!("Command bytes: {:02X?}", command_bytes);
+        self.stream.write_all(&command_bytes)?;
+        info!("Sent {} bytes", command_bytes.len());
+        Ok(())
+    }
+
+    fn receive_response(&mut self) -> io::Result<Vec<u8>> {
+        let mut buffer = [0u8; 1024];
+        match self.stream.read(&mut buffer) {
+            Ok(bytes_received) => {
+                info!(
+                    "Received {} bytes: {:02X?}",
+                    bytes_received,
+                    &buffer[..bytes_received]
+                );
+                Ok(buffer[..bytes_received].to_vec())
+            }
+            Err(e) => {
+                error!("Failed to receive response: {}", e);
+                Err(e)
+            }
+        }
+    }
+}
+
+pub fn send_command_and_wait(
+    transport: &mut Box<dyn ViscaTransport>,
+    command: &ViscaCommand,
+) -> io::Result<()> {
+    transport.send_command(command)?;
+
+    loop {
+        match transport.receive_response() {
+            Ok(response) => {
+                display_camera_response(&response);
+                // Check if the response is 4 bytes or, if it has 3 bytes, check whether the second byte is in the range 0x50 to 0x5F
+                if response.len() == 4
+                    || (response.len() == 3 && (0x50..=0x5F).contains(&response[1]))
+                {
+                    break;
+                }
+            }
+            Err(e) => {
+                error!("Failed to receive response: {}", e);
+                return Err(e);
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn display_camera_response(response: &[u8]) {
+    let mut i = 0;
+    while i < response.len() {
+        if response[i] == 0x90 {
+            match response.get(i..i + 3) {
+                Some([0x90, code, 0xFF]) if (0x40..=0x4F).contains(code) => {
+                    info!("ACK: Command accepted with status code: {:02X?}", code);
+                    i += 3;
+                }
+                Some([0x90, code, 0xFF]) if (0x50..=0x5F).contains(code) => {
+                    info!(
+                        "Completion: Command executed with status code: {:02X?}",
+                        code
+                    );
+                    i += 3;
+                }
+                Some([0x90, 0x60, 0x02, 0xFF]) => {
+                    error!("Error: Syntax Error.");
+                    i += 4;
+                }
+                Some([0x90, 0x60, 0x03, 0xFF]) => {
+                    error!("Error: Command Buffer Full.");
+                    i += 4;
+                }
+                Some([0x90, code, 0x04, 0xFF]) if (0x60..=0x6F).contains(code) => {
+                    error!("Error: Command Canceled.");
+                    i += 4;
+                }
+                Some([0x90, code, 0x05, 0xFF]) if (0x60..=0x6F).contains(code) => {
+                    error!("Error: No Socket.");
+                    i += 4;
+                }
+                Some([0x90, code, 0x41, 0xFF]) if (0x60..=0x6F).contains(code) => {
+                    error!("Error: Command Not Executable.");
+                    i += 4;
+                }
+                _ => {
+                    error!("Unknown response: {:02X?}", &response[i..i + 3]);
+                    i += 3;
+                }
+            }
+        } else {
+            error!("Unexpected response format: {:02X?}", &response[i..]);
+            break;
+        }
+    }
+}
