8bcf89f57e5d14d19820f8c41261936d7e44cc3d
Grant Sparks <grant@sparks.to>
2024-08-13 08:19:35 +1000

    Command for managing presets

---
+++ src/command/preset.rs
@@ -0,0 +1,40 @@
+use crate::command::ViscaCommand;
+use crate::error::ViscaError;
+
+use super::ViscaResponseType;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum PresetAction {
+    Reset = 0x00,
+    Set = 0x01,
+    Recall = 0x02,
+}
+
+pub struct PresetCommand {
+    pub action: PresetAction,
+    pub preset_number: u8, // 0x00 to 0xFE (0 to 254)
+}
+
+impl ViscaCommand for PresetCommand {
+    fn to_bytes(&self) -> Result<Vec<u8>, ViscaError> {
+        if self.preset_number <= 0xFE {
+            Ok(vec![
+                0x81,
+                0x01,
+                0x04,
+                0x3F,
+                self.action as u8,
+                self.preset_number,
+                0xFF,
+            ])
+        } else {
+            Err(ViscaError::InvalidParameter(
+                "Preset number must be in the range 0x00..=0xFE".into(),
+            ))
+        }
+    }
+
+    fn response_type(&self) -> Option<ViscaResponseType> {
+        None
+    }
+}
