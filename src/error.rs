3ad28bfe33937b5a405c34a5e9de5ae89d84daf4
Grant Sparks <grant@sparks.to>
2025-06-04 04:41:58 +0000

    refactor: remove non_exhaustive from ViscaError for better ergonomics
    - Traditional exhaustive enum provides better user experience
    - VISCA protocol has well-defined error conditions unlikely to change
    - Users can handle all error cases with compile-time guarantees
    - Accept that new error variants require major version bumps

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,10 @@
 use std::io;
 use thiserror::Error;
 
-#[non_exhaustive]
+// Using traditional exhaustive enum approach for ViscaError
+// This provides better ergonomics for library users who need to handle specific errors
+// The VISCA protocol has a well-defined set of error conditions that are unlikely to change frequently
+// Adding new variants will require a major version bump, which is acceptable for this use case
 #[derive(Error, Debug)]
 pub enum ViscaError {
     #[error("IO error: {0}")]
