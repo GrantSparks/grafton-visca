d205dcf52468c47745a7f2882c427b2f5b0d0263
Grant Sparks <grant@sparks.to>
2025-07-11 12:22:52 +0000

    refactor: remove unused macros from codebase
    - Remove execute_command! and visca_param_command! declarative macros
    - Remove unused procedural macros from grafton-visca-macros crate
    - Keep only ViscaValue and InquiryCommand derive macros that are actively used
    - Update macro documentation and exports accordingly
    - Delete test files for removed macros
    - Simplify grafton-visca-macros to only essential functionality

--- grafton-visca-macros/src/lib.rs
+++ grafton-visca-macros/src/lib.rs
@@ -4,118 +4,19 @@
 
 //! Procedural macros for the grafton-visca library
 //!
-//! This crate provides derive and attribute macros to simplify common patterns
+//! This crate provides derive macros to simplify common patterns
 //! in VISCA command implementations.
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
-mod bounded_macros;
-mod command_macros;
 mod inquiry_command;
-mod method_macros;
 mod parser_templates;
-mod position_macros;
-mod speed_macros;
-mod test_macros;
 mod value_macros;
 
-#[proc_macro_attribute]
-pub fn visca_method(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_method(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_method_custom(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_method_custom(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_camera_method(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_camera_method(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_command(attr: TokenStream, item: TokenStream) -> TokenStream {
-    command_macros::visca_command(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_command_variants(attr: TokenStream, item: TokenStream) -> TokenStream {
-    command_macros::visca_command_variants(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_method_generic(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_method_generic(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_inquiry(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_inquiry(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_position_command(attr: TokenStream, item: TokenStream) -> TokenStream {
-    position_macros::visca_position_command(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_speed_command(attr: TokenStream, item: TokenStream) -> TokenStream {
-    speed_macros::visca_speed_command(attr, item)
-}
-#[proc_macro_attribute]
-pub fn visca_bounded_command(attr: TokenStream, item: TokenStream) -> TokenStream {
-    bounded_macros::visca_bounded_command(attr, item)
-}
 #[proc_macro_derive(ViscaValue, attributes(visca_value))]
 pub fn derive_visca_value(input: TokenStream) -> TokenStream {
     value_macros::derive_visca_value(input)
-}
-
-#[proc_macro_attribute]
-pub fn visca_fallible_method(attr: TokenStream, item: TokenStream) -> TokenStream {
-    method_macros::visca_fallible_method(attr, item)
-}
-
-#[proc_macro_attribute]
-pub fn visca_mock_transport(attr: TokenStream, item: TokenStream) -> TokenStream {
-    test_macros::visca_mock_transport(attr, item)
-}
-/// Generate validation tests for VISCA commands.
-///
-/// This macro generates comprehensive test suites that validate:
-/// - Command byte sequences against documentation
-/// - Parameter bounds for all camera profiles
-/// - Response parsing for all command types
-/// - Error cases and edge conditions
-///
-/// # Example
-///
-/// ```rust,ignore
-/// #[visca_test_suite]
-/// mod zoom_command_tests {
-///     use super::*;
-///     
-///     #[test_command(
-///         command = "ZoomCommand::Direct(ZoomPosition::new(0x4000)?)",
-///         expected_bytes = "[0x81, 0x01, 0x04, 0x47, 0x04, 0x00, 0x00, 0x00, 0xFF]",
-///         profiles = ["PTZOpticsG2", "SonyEVID70"]
-///     )]
-///     fn test_zoom_direct() {}
-///     
-///     #[test_bounds(
-///         parameter = "zoom_position",
-///         min = "0x0000",
-///         max = "0x7000",
-///         profiles = ["PTZOpticsG2"]
-///     )]
-///     fn test_zoom_bounds() {}
-/// }
-/// ```
-#[proc_macro_attribute]
-pub fn visca_test_suite(attr: TokenStream, item: TokenStream) -> TokenStream {
-    test_macros::visca_test_suite(attr, item)
 }
 
 /// Derive macro for generating InquiryCommand implementations with parser support
